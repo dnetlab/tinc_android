e2a4269b4ca1602442d8bcf2dc6de18061dddb5c
lei.dai <lei.dai@dragonflow.com>
2019-03-27 16:29:00 +0800

    First commit

---
+++ jni/Application.mk
@@ -0,0 +1,7 @@
+#APP_STL := gnustl_static
+APP_STL := stlport_static
+NDK_TOOLCHAIN_VERSION :=4.9
+#APP_ABI := armeabi
+APP_ABI := armeabi armeabi-v7a
+APP_SHORT_COMMANDS := true
+APP_PLATFORM :=android-16
